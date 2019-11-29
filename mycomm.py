2c70074ee4ae76acacf2685d83c6cc7f223dae6b
Wataru Kuwabara <56509698+jhon813ball@users.noreply.github.com>
2019-11-29 11:43:40 +0900

    test commit

---
+++ mycomm.py
@@ -0,0 +1,21 @@
+﻿import numpy as np
+
+class CommPath:
+    
+    def __init__(self):
+        self.L = 0
+        
+    def awgn(self, x, snr_db):
+        L = x.size
+        snr_true = 10.0**(snr_db/10.0)
+        Esym = sum(np.abs(x)**(2))/L
+        N0 = Esym/snr_true
+        if isinstance(x, complex):
+            noiseSigma = np.sqrt(N0/2.0)
+            n = noiseSigma*(np.random.randn(L)+1j*np.random.randn(L))
+        else:
+            noiseSigma = np.sqrt(N0)
+            n = noiseSigma*np.random.randn(L)
+        y = x + n
+        return y
+    
